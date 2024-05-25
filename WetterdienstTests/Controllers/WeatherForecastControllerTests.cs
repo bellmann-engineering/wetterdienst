b45af1bc4d8c79deb833413371bf6976511bdf4e
Kai <kb@bellmann-engineering.com>
2024-05-25 10:06:37 +0200

    added solution

---
+++ WetterdienstTests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Wetterdienst.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wetterdienst.Controllers.Tests
+{
+	[TestClass()]
+	public class WeatherForecastControllerTests
+	{
+		[TestMethod()]
+		public void GetCorrectNumberOfWeatherForecatsTest()
+		{
+			// A A A
+			// Arrange
+			WeatherForecastController controller = new WeatherForecastController();
+			int expected = 5;
+			// Act
+			int actual = controller.Get().Count();
+
+			// Assert
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
